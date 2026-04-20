440cb6cade2fff603dce381a5b692eddcbf5caed
Saurabh Kumar <Saurabh.Kumar@grapecity.com>
2026-04-20 16:14:58 +0530

    Remove comment 13

--- Program.cs
+++ Program.cs
@@ -25,7 +25,6 @@ if (!app.Environment.IsDevelopment())
 //Add comment 10 - saurabh
 //Add comment 11 - saurabh
 //Add comment 12 - saurabh
-//Add comment 13 - saurabh
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
