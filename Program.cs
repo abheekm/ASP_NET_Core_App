d8702d4fbb0e5605330f08c864905bade04d861d
Saurabh Kumar <Saurabh.Kumar@grapecity.com>
2026-05-05 16:53:46 +0530

    Comment the new code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -10,7 +10,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 //Add comment test kk test testing
 //Add comment - saurabh
