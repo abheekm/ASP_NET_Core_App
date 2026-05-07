f6d781989d6518a20c074045560ec8250e477602
Abheek Mukherjee <abheek.mukherjee@grapecity.com>
2026-05-07 17:00:37 +0530

    test

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,14 +5,11 @@ namespace ASP_NET_Core_App.Pages
 {
     public class IndexModel : PageModel
     {
-        //private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            Console.WriteLine(userName);
-            string name = null;
-            int len = name.Length;
         }
 
         public void OnGet()
