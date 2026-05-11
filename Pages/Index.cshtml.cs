1cebd6ea1cfd04b91999c159854a12c1c555a70a
Saurabh Kumar <Saurabh.Kumar@grapecity.com>
2026-05-11 18:23:41 +0530

    Induce some errors

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ namespace ASP_NET_Core_App.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
